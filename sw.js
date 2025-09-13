8d5c8f487b3a89b726f78de37b8fa322883c63e9
kushagrasharma <fourwheelerinsurance@gmail.com>
2025-09-14 00:34:25 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,9 +1,49 @@
-// sw.js
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installing...');
+const CACHE_NAME = 'uscustomers-v1';
+const urlsToCache = [
+  './',
+  './_expo/static/js/web/entry-*.js',
+  './_expo/static/css/web-*.css',
+  './assets/images/icon.png',
+  './assets/images/favicon.png'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    // You can add caching logic here
-    console.log('Service Worker fetching:', event.request.url);
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+  // Remove old caches
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
